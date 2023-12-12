2055966a56982bcd5cbcd0722b8114760d91f130
nehajoshi1729 <132944854+nehajoshi1729@users.noreply.github.com>
2023-12-12 15:58:30 -0500

    Update helloserver.js classwork

--- basics/helloserver.js
+++ basics/helloserver.js
@@ -10,6 +10,12 @@ const server = http.createServer((req, res) => {
     res.end('Hello World\n')
 })
 
+app.get('/home') => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Home Page\n')
+})
+
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
 })
